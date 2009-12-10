258a8eda921c8bcd19603e7964aeb47fce650ccc
doncarloknute <carl.knutson@gmail.com>
2009-12-10 16:43:37 -0600

    Fixed IP Locations CSV; extracted US entries; opend result in FasterCSV

--- lib/IPCensus.rb
+++ lib/IPCensus.rb
@@ -1,11 +1,23 @@
 require 'rubygems'
 require 'geoip'
+require 'fastercsv'
 
-ip_path = '/data/ripd/GeoLiteCity.dat'
+# Uses the GeoIP gem to look up a data entry about a certain IP or host.
+#
+#ip_path = '/data/ripd/GeoLiteCity.dat'
+#
+#ip_data = GeoIP.new(ip_path)
+#
+#print "Enter a host or IP address:"
+#ip = gets.strip
+#
+#p ip_data.city(ip)
 
-ip_data = GeoIP.new(ip_path)
+data_path = '/data/ripd/GeoLiteCity_20091201/'
+ip_locations = data_path + 'GeoLiteCity-USLocation.csv'
 
-print "Enter a host or IP address:"
-ip = gets.strip
-
-p ip_data.city(ip)
+FasterCSV.foreach(ip_locations) do |row|
+  if row[3] == "Austin"
+    p row
+  end
+end
