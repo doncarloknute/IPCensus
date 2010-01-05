a72f35cfb2508ca3bc74376ac1ae0f16ec8be7bb
doncarloknute <carl.knutson@gmail.com>
2010-01-04 23:33:34 -0600

    Rejected any non-numeric zips. Matched zip-like to logical record number.

--- lib/MatchLogRecNum.rb
+++ lib/MatchLogRecNum.rb
@@ -2,11 +2,10 @@ require 'rubygems'
 require 'fastercsv'
 
 # Main data path
-#data_path = '/data/ripd/www2.census.gov/census_2000/datasets/Summary_File_3/0_National/'
-#data_path = '/data/ripd/GeoLiteCity_20091201/'
+data_path = '/data/ripd/GeoLiteCity_20091201/'
 
 # Test data path
-data_path = '/data/temp/minidata/'
+#data_path = '/data/temp/minidata/'
 
 # Geo data file
 geo_data = data_path + 'usgeo.uf3'
@@ -30,7 +29,7 @@ IO.foreach(geo_data) do |entry|
   georow[2] = entry[292..300].to_i  #population
   georow[3] = entry[310..318].insert(3, '.').to_f  #latitute
   georow[4] = entry[319..328].insert(4, '.').to_f  #longitude
-  if georow[1] != '     '
+  if georow[1] != '     ' and georow[1][3..4] != 'HH' and georow[1][3..4] != 'XX'
     if geozip_hash.key?(georow[1])
       p georow[1]
       raise Exception, "Multiple entries for a ZCTA5 code."
@@ -45,10 +44,14 @@ FasterCSV.foreach(iploc_data) do |row|
   outrow = []
   outrow[0] = row[0] #IP block start num
   outrow[1] = row[1] #IP block end num
-  outrow[2] = "#{(row[0]/16777216)%256}.#{(row[0]/65536)%256}.#{(row[0]/256)%256}.#{row[0]%256}" #IP block start num in string format
-  outrow[3] = "#{(row[1]/16777216)%256}.#{(row[1]/65536)%256}.#{(row[1]/256)%256}.#{row[1]%256}" #IP block end num in string format
+  outrow[2] = "#{(row[0].to_i/16777216)%256}.#{(row[0].to_i/65536)%256}.#{(row[0].to_i/256)%256}.#{row[0].to_i%256}" #IP block start num in string format
+  outrow[3] = "#{(row[1].to_i/16777216)%256}.#{(row[1].to_i/65536)%256}.#{(row[1].to_i/256)%256}.#{row[1].to_i%256}" #IP block end num in string format
   outrow[4] = row[5] #ZIP code associated with IP block
-  outrow[5] = geozip_hash[row[5]][1] #ZIPlike number associated with IP block
+  if geozip_hash.key?(row[5])
+	outrow[5] = geozip_hash[row[5]][1] #ZIPlike number associated with IP block
+  else
+	outrow[5] = nil
+  end
   outrow[6] = row[2] #country
   outrow[7] = row[3] #state
   outrow[8] = row[4] #city
@@ -61,5 +64,15 @@ FasterCSV.foreach(iploc_data) do |row|
   end
 end
 
-geozip_hash.clear
+# Write out the matching ZIPLIKE numbers with their corresponding Logical Record Number
+geozip_hash.sort.each do |entry|
+  outrow = []
+  outrow[0] = entry[0]
+  outrow[1] = entry[1][0]
+  FasterCSV.open(zip_logrec, "a", options = tab_sep) do |csv|
+    csv << outrow
+  end
+end
 
+
+geozip_hash.clear
