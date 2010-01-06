2886587f9a527eb1e72b571f7a9c1c2c4559c03d
doncarloknute <carl.knutson@gmail.com>
2010-01-05 23:19:06 -0600

    Changed to process all uf3 files in a dir.

--- lib/ParseSF3LRN.rb
+++ lib/ParseSF3LRN.rb
@@ -13,9 +13,9 @@ census_path = '/data/ripd/www2.census.gov/census_2000/datasets/Summary_File_3/O_
 # ZIPLIKE and Logical Record Number file
 zip_logrec = data_path + 'zip_codes_to_logical_record_number.tsv'
 # SF3 data file
-sf3_data = census_path + 'us00001.uf3'
+#sf3_data = census_path + 'us00001.uf3'
 # data file with only selected logical record numbers
-out_data = data_path + 'census_2000_sf3_zip_us00001.tsv'
+#out_data = data_path + 'census_2000_sf3_zip_us00001.tsv'
 # Options hash for tab separated values
 tab_sep = {:col_sep => "\t"}
 # Options hash to force quote entries
@@ -26,14 +26,19 @@ FasterCSV.foreach(zip_logrec, options = tab_sep) do |row|
   ziplike_hash[row[1]] = row[0]
 end
 
-FasterCSV.foreach(sf3_data, options = force_quote) do |row|
-  outrow = []
-  if ziplike_hash.key?(row[4])
-    outrow[0] = ziplike_hash[row[4]] #put ziplike as first item
-	outrow += row[4..-1] #put logical record number and all other data as the rest
-    FasterCSV.open(out_data, "a", options = tab_sep) do |csv|
-	  csv << outrow
-	end
+Dir.chdir(census_path)
+Dir.glob("us000??.uf3") do |filename|
+  sf3_data = census_path + filename
+  out_data = data_path + "census_2000_sf3_zip_us000" + filename[5..6] + ".tsv"
+  FasterCSV.foreach(sf3_data, options = force_quote) do |row|
+    outrow = []
+    if ziplike_hash.key?(row[4])
+      outrow[0] = ziplike_hash[row[4]] #put ziplike as first item
+      outrow += row[4..-1] #put logical record number and all other data as the rest
+      FasterCSV.open(out_data, "a", options = tab_sep) do |csv|
+        csv << outrow
+      end
+    end
   end
 end
 
