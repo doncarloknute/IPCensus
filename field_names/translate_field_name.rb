e46ef106bdec2830410abaa364808e9bceef08b0
Carl Knutson <carl@infochimps.org>
2010-01-11 17:11:03 -0600

    Regexp field names.

--- field_names/translate_field_name.rb
+++ field_names/translate_field_name.rb
@@ -1,7 +1,20 @@
 
 
-/(\$\d+),(\d+)[^\d]/
-/(\$\d+),(\d+),(\d+)[^\d]/
+# /(\$\d+),(\d+)[^\d]/
+# /(\$\d+),(\d+),(\d+)[^\d]/
 
-[ /$(\d+) to $(\d+)/,                        'D\1_\2'
-[ /percent of total income/,                 '']
+#[ /$(\d+) to $(\d+)/,                        'D\1_\2']
+#[ /percent of total income/,                 '']
+
+
+raw_counts = 'raw_with_counts.tsv'
+
+IO.foreach(raw_counts) do |line|
+  if line =~ /(\$\d+),(\d+)/
+    line.chomp!.delete!(",")
+    p line.gsub(/\$(\d+) to \$(\d+)/, 'D\1_\2')
+    if line =~ /$(\d+) to $(\d+)/
+      p line
+    end
+  end
+end
