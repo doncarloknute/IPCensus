b913e5b3fc18cbf27c7b430ea0e0cf20884cef97
Carl Knutson <carl@infochimps.org>
2010-01-13 16:57:11 -0600

    Modified field names with money ranges.

--- field_names/translate_field_name.rb
+++ field_names/translate_field_name.rb
@@ -7,27 +7,34 @@ require 'fastercsv'
 #[ /$(\d+) to $(\d+)/,                        'D\1_\2']
 #[ /percent of total income/,                 '']
 
+class String
+  def gsub_bs! regex, repl
+    if repl.is_a?(String) then gsub!(regex, repl)
+    else                       gsub!(regex, &repl) end
+  end
+end
 
 raw_counts = 'raw_with_counts.tsv'
 
-IO.foreach(raw_counts) do |line|
+File.open(raw_counts).each do |line|
+  line.chomp!
   fixed_line = ''
   if line =~ /(\$\d+),(\d+)/
-    line.chomp!
     #p line
     # line.gsub!(/\$(\d+),(\d+) to \$(\d+),(\d+)/, 'Usd\1kto\3k')
-    {
-      /\$?(\d+)(?:,\d+)? to \$?(\d+)(?:,\d+) or loss/ => 'Usd\1kto\2korL', # eg $1 to $2,499 or loss
-      /\$?(\d+)(?:,\d+)? to \$?(\d+)(?:,\d+)/ => 'Usd\1kto\2k', # eg10,000 to $12,499
-      /\$?(\d+)(?:,\d+)? or more/ => 'Usd\1korM', # $200,000 or more 
-      /Less than \$?(\d+)(?:,\d+)/ => 'LtUsd\1k'
-    }.each do |regex, repl|
-      fixed_line = line.gsub(regex, repl) and break
+    orig_line = line.dup
+    [
+      [/\$?(\d+)(?:,\d+)? to \$?(\d+)(?:,\d+) or loss/, 'Usd\1kto\2korL'], # eg $1 to $2,499 or loss
+      [/\$?(\d+)(?:,\d+)? to \$?(\d+)(?:,\d+)/,         Proc.new{ "Usd#{$1}kto#{$2.to_i+1}k"}], # eg $10,000 to $12,499
+      [/\$?(\d+)(?:,\d+)? or more/, 'Usd\1korM'], # eg $200,000 or more 
+      [/Less than \$?(\d+)(?:,\d+)/, 'LtUsd\1k'], # eg Less than $10,000
+    ].each do |regex, repl|
+      line.gsub_bs!(regex, repl) and break
     end
-    if true || line == fixed_line
-      p [line, fixed_line]
+    if true || line == orig_line
+      p [orig_line, line]
     end
-    puts fixed_line
+    # puts line
   end
   if line =~ /[Hh]ouseholder\s?/
     fixed_line = line.gsub(/[Hh]ouseholder\s?/, 'Hh')
