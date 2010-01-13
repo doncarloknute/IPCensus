e2184d63a3dceba771166984f2d4f91cb7d26203
Carl Knutson <carl@infochimps.org>
2010-01-13 16:11:47 -0600

    Modified field names with Householder and age range.

--- field_names/translate_field_name.rb
+++ field_names/translate_field_name.rb
@@ -1,4 +1,5 @@
-
+require 'rubygems'
+require 'fastercsv'
 
 # /(\$\d+),(\d+)[^\d]/
 # /(\$\d+),(\d+),(\d+)[^\d]/
@@ -10,11 +11,40 @@
 raw_counts = 'raw_with_counts.tsv'
 
 IO.foreach(raw_counts) do |line|
+  fixed_line = ''
   if line =~ /(\$\d+),(\d+)/
-    line.chomp!.delete!(",")
-    p line.gsub(/\$(\d+) to \$(\d+)/, 'D\1_\2')
-    if line =~ /$(\d+) to $(\d+)/
-      p line
+    line.chomp!
+    #p line
+    # line.gsub!(/\$(\d+),(\d+) to \$(\d+),(\d+)/, 'Usd\1kto\3k')
+    {
+      /\$?(\d+)(?:,\d+)? to \$?(\d+)(?:,\d+) or loss/ => 'Usd\1kto\2korL', # eg $1 to $2,499 or loss
+      /\$?(\d+)(?:,\d+)? to \$?(\d+)(?:,\d+)/ => 'Usd\1kto\2k', # eg10,000 to $12,499
+      /\$?(\d+)(?:,\d+)? or more/ => 'Usd\1korM', # $200,000 or more 
+      /Less than \$?(\d+)(?:,\d+)/ => 'LtUsd\1k'
+    }.each do |regex, repl|
+      fixed_line = line.gsub(regex, repl) and break
     end
+    if true || line == fixed_line
+      p [line, fixed_line]
+    end
+    puts fixed_line
+  end
+  if line =~ /[Hh]ouseholder\s?/
+    fixed_line = line.gsub(/[Hh]ouseholder\s?/, 'Hh')
+    if line == fixed_line
+      p [line, fixed_line]
+    end
+    puts fixed_line
+  end
+  if line =~ /(\d\d) to (\d\d) years/
+    if fixed_line == ''
+      fixed_line = line.gsub(/(\d\d) to (\d\d) years/, '\1to\2y')
+    else
+      fixed_line.gsub!(/(\d\d) to (\d\d) years/, '\1to\2y')
+    end
+    if line == fixed_line
+      p [line, fixed_line]
+    end
+    puts fixed_line
   end
 end
