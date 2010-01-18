6a5607f32d27ab264de6c4e8541c1a852ff0c6ac
Carl Knutson <carl@infochimps.org>
2010-01-18 16:18:49 -0600

    Adding universe entries

--- field_names/extract_field_names.sh
+++ field_names/extract_field_names.sh
@@ -4,3 +4,10 @@ cat sas_files_ripd/SF3/*.sas |
     puts [$3].join("\t") if ($_ =~ %r{^         ([A-Z]+\d{6})=\W(\s*)(.+)'"'"'})' |
     wu-hist  | sort -t"   " -k2
     > field_names/raw_with_counts.tsv
+
+	cat sas_files_ripd/SF3/*.sas |
+	  ruby -ne '
+	    $_.chomp! ;
+	    puts [$1, $2.length, $3].join("\t") if ($_ =~ %r{^         ([A-Z]+\d{3}[A-Z]?\d{3})=\W(\s*)(.+)'"'"'})' |
+	    wu-hist  | sort -t"   " -k2
+	    > field_names/raw_with_counts.tsv
