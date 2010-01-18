b1866fb8e3c2d556b1a5c5ab2d29e6e819776dfd
Carl Knutson <carl@infochimps.org>
2010-01-18 16:56:30 -0600

    Extracting all field names and universe labels

--- field_names/extract_field_names.sh
+++ field_names/extract_field_names.sh
@@ -11,3 +11,18 @@ cat sas_files_ripd/SF3/*.sas |
 	    puts [$1, $2.length, $3].join("\t") if ($_ =~ %r{^         ([A-Z]+\d{3}[A-Z]?\d{3})=\W(\s*)(.+)'"'"'})' |
 	    wu-hist  | sort -t"   " -k2
 	    > field_names/raw_with_counts.tsv
+	
+cat sas_files_ripd/SF3/*.sas | 
+	ruby -ne '
+		$_.chomp! ; 
+		puts [$3].join("\t") if ($_ =~ %r{^         (\/\*)(Universe):\s+(.+)(\*\/)}) ; 
+		puts [$3].join("\t") if ($_ =~ %r{^         ([A-Z]+\d{3}[A-Z]?\d{3})=\W(\s*)(.+)'"'"'})' 
+		> field_names/raw_universe.tsv
+		
+cat sas_files_ripd/SF3/*.sas | 
+	ruby -ne '
+		$_.chomp! ; 
+		puts [$2, $3].join("\t") if ($_ =~ %r{^         (\/\*)(Universe):\s+(.+)(\*\/)}) ; 
+		puts [$1, $2.length, $3].join("\t") if ($_ =~ %r{^         ([A-Z]+\d{3}[A-Z]?\d{3})=\W(\s*)(.+)'"'"'})' 
+		> field_names/raw_with_field_code_universe.tsv
+
