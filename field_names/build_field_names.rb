3608fd5920263b069cb0277688eb25a66289525c
Carl Knutson <carl@infochimps.org>
2010-01-22 17:04:56 -0600

    Found long field names

--- field_names/build_field_names.rb
+++ field_names/build_field_names.rb
@@ -1,9 +1,9 @@
-require 'rubygems'
-require 'fastercsv'
 
 mod_field_names = 'mod_universe_with_counts_aligned.tsv'
-#codes_names = 'raw_with_field_code_universe.tsv'
-codes_names = 'test.tsv'
+codes_names = 'raw_with_field_code_universe.tsv'
+
+const_field_names = File.open("constructed_field_names.tsv", "w")
+long_names = File.open("long_field_names.tsv", "w")
 
 field_hash = Hash.new
 
@@ -26,11 +26,12 @@ File.open(codes_names).each do |line|
   else
     depth = (row[1].dup.rstrip.to_i - 4) / 2
   end
-  puts row.join("\t")
+#  puts row.join("\t")
   orig_field = row[2].rstrip
   name_array[depth] = field_hash[orig_field]
   final_name = name_array[0..depth].join("_")
-  puts final_name + "\t" + "#{final_name.length}" if row[0].rstrip != "Universe"
+  const_field_names << row[0] + "\t" + final_name + "\t" + orig_field + "\n" if row[0].rstrip != "Universe"
+  long_names << final_name + "\t" + "#{final_name.length}" + "\n" if final_name.length > 64
 end
 
 field_hash.clear
