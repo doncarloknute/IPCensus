721e0c3f8987659b67629cab591f84ff86e39c8e
Carl Knutson <carl@infochimps.org>
2010-01-25 16:50:02 -0600

    Language/Ancestory field tweaks

--- field_names/build_field_names.rb
+++ field_names/build_field_names.rb
@@ -16,6 +16,8 @@ File.open(mod_field_names).each do |line|
 #  puts mod_field + "\t" + orig_field
 end
 
+#yaml_file = "SF3" + ('%02d' % file_index) + '.icss.yaml'
+
 name_array = []
 long_index = 0
 
@@ -29,6 +31,7 @@ File.open(codes_names).each do |line|
   end
   orig_field = row[2].rstrip
   name_array[depth] = field_hash[orig_field]
+  name_array[depth] = row[2].rstrip if (name_array[0] == 'PopGte5y' && name_array[depth] =~ /Anc\d{3}/)
   final_name = name_array[0..depth].join("_")
   if final_name.length > 64
     long_names << row[0] + "\t" + ('%s_%03d' % [final_name[0..60], long_index]) + "\t" + final_name + "\n"
