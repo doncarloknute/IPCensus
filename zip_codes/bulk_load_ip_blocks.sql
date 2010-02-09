e1ed0054c56e0d313da1672aa644955cd39a79c1
Carl Knutson <carl@infochimps.org>
2010-02-08 18:04:08 -0600

    Added locations table

--- zip_codes/bulk_load_ip_blocks.sql
+++ zip_codes/bulk_load_ip_blocks.sql
@@ -11,3 +11,17 @@ LOAD DATA INFILE '/Users/doncarlo/ics/Infochimps/IPCensus/geoip_ripd/GeoLiteCity
 ALTER TABLE ip_blocks ENABLE KEYS;
 
 SELECT COUNT(*), 'ip_blocks' FROM ip_blocks ;
+
+
+ALTER TABLE locations DISABLE KEYS;
+
+LOAD DATA INFILE '/Users/doncarlo/ics/Infochimps/IPCensus/geoip_ripd/GeoLiteCity-Location.csv' 
+   REPLACE INTO TABLE locations
+   FIELDS TERMINATED BY "," ENCLOSED BY '"'
+   IGNORE 2 LINES
+   (location_id, country_code, region_code, city, postal_code, latitude, longitude, metro_code, area_code)
+;
+
+ALTER TABLE locations ENABLE KEYS;
+
+SELECT COUNT(*), 'locations' FROM locations ;
--- zip_codes/ip_address_mapper.rb
+++ zip_codes/ip_address_mapper.rb
@@ -23,8 +23,8 @@ class IPLogEntry
   
   property :counts, Integer
   # min > 0 forces unsigned in dbs that support it
-  property :ipaddress, Integer, :key => true, :min => 0  
-  property :useragent, String, :length => 255, :key => true
+  property :ip_address, Integer, :key => true, :min => 0  
+  property :user_agent, String, :length => 255, :key => true
   
 end
 
@@ -34,10 +34,29 @@ class IPBlock
   property :start_ip,    Integer, :min => 0, :key => true,    :unique_index => :ip_range
   property :end_ip,      Integer, :min => 0,                  :unique_index => :ip_range
   property :location_id, Integer
+  
+#  has 1, :location, :through => :location_id
+end
+
+class Location
+  include DataMapper::Resource
+  
+  property :location_id,  Integer, :key => true
+  property :country_code, String
+  property :region_code,  String
+  property :city,         String
+  property :postal_code,  String
+  property :latitude,     Float
+  property :longitude,    Float
+  property :metro_code,   Integer
+  property :area_code,    Integer
+  
+#  has n, :ipblocks, :through => :location_id
 end
 
 IPLogEntry.auto_migrate!
 IPBlock.auto_migrate!
+Location.auto_migrate!
 
 # --------------------------------------------------------------------------
 
@@ -57,8 +76,27 @@ File.open(ip_log_path + 'IP_20100124.tsv').each do |line|
   packed_ip = dotted_ip_to_packed_ip(dotted_ip)
   user_agent = user_agent[0..254]
   #p packed_ip
-  ip_row = IPLogEntry.create :counts => counts, :ipaddress => packed_ip, :useragent => user_agent
+  ip_row = IPLogEntry.create :counts => counts, :ip_address => packed_ip, :user_agent => user_agent
 end
+
+#
+# File.open(geoip_path + 'GeoLiteCity-Location.csv').each do |line|
+#   line.chomp!
+#   unless line =~ /^\d+.+/ then warn "Bad line: #{line}" ; next ; end
+#   locid, ccode, rcode, city, pcode, lat, long, mcode, acode = line.split(",")
+#   locid = locid.to_i
+#   ccode.delete!('"') if ccode.is_a?(String) 
+#   rcode.delete!('"') if rcode.is_a?(String) 
+#   city.delete!('"') if city.is_a?(String) 
+#   pcode.delete!('"') if pcode.is_a?(String) 
+#   lat = lat.to_f
+#   long = long.to_f
+#   mcode = mcode.to_i if mcode != nil
+#   acode = acode.to_i if acode != nil
+#   locale = Location.create :location_id => locid, :country_code => ccode, :region_code => rcode, :city => city, :postal_code => pcode, :latitude => lat, :longitude => long, :metro_code => mcode, :area_code => acode
+# end
+#
+#
 # 
 # File.open(geoip_path + 'GeoLiteCity-Blocks.csv').each do |line|
 #   line.chomp!
